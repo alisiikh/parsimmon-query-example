5a1f683a9633708bfbce3ce55eb9c88f508c7c8c
Aleksey Lisikh <lizard5472@gmail.com>
2017-09-18 03:41:33 +0200

    clean up, and improved language

--- QueryLang.js
+++ QueryLang.js
@@ -1,30 +1,56 @@
 let P = require('Parsimmon');
 
-let token = (parser) => parser.skip(P.optWhitespace);
+let whitespace = P.optWhitespace.desc("whitespace (separator)");
+/**
+ * Token that might be wrapped with whitespaces.
+ * @param parser
+ */
+let token = (parser) => parser.skip(whitespace);
+/**
+ * A string that might be wrapped with whitespaces.
+ * @param str
+ */
 let word = (str) => P.string(str).thru(token);
+let mapExpr = (results) => {
+    return {
+        "var": results[0],
+        "operation": results[1],
+        "value": results[2]
+    }
+};
+
 
 let QueryLang = P.createLanguage({
-    fullExpr: (r) => P.alt(r.expr, r.valueExpr).sepBy(r.separator).desc("full expression"),
-    expr: function (r) {
-        return P.seq(r.variable.skip(r._), r.operator.skip(r._), r.value).map(function (results) {
-            if (results.length === 1) {
-                // this is a value expression
-                return results[0];
-            } else {
-                // this is a complex expr, e.g. key=value
-                return {
-                    "var": results[0],
-                    "operator": results[1],
-                    "value": results[2]
-                }
-            }
+    /**
+     * Matches a whole expression
+     */
+    fullExpr: (r) => P.alt(r.complexExpr, r.valueExpr)
+        .sepBy(whitespace)
+        .desc("full expression"),
 
-        }).desc("complex expression");
-    },
-    valueExpr: (r) => r.quotedString.map((results) => {
-        return { var: "*", operator: "=", "value": results }
-    }).desc("value expression"),
-    variable: () => P.regex(/[a-zA-Z_]\w*/).desc("variable"),
+    /**
+     * Matches a key-value expr, e.g. key="value", key=43
+     */
+    complexExpr: (r) => P.seq(r.variable, r.operator, r.value)
+        .thru(parser => whitespace.then(parser))
+        .map(mapExpr)
+        .desc("complex expression"),
+
+    /**
+     * Matches a value expr, e.g. "value"
+     */
+    valueExpr: (r) => r.quotedString
+        .thru(parser => whitespace.then(parser))
+        .map((value) => mapExpr(["*", "=", value]))
+        .desc("value expression"),
+
+    /**
+     * Matches a variable
+     */
+    variable: () => token(P.regex(/[a-zA-Z_]\w*/)).desc("variable"),
+    /**
+     * Matches allowed operator
+     */
     operator: () => P.alt(
         word("="),
         word("!="),
@@ -33,24 +59,35 @@ let QueryLang = P.createLanguage({
         word("<"),
         word(">")
     ).desc("operator"),
+    /**
+     * Matches a value
+     * @param r
+     */
     value: (r) => P.alt(
         r.quotedString,
         r.decimalNumber,
         r.number,
-        r.array).desc("value"),
-    separator: () => P.whitespace.desc("separator"),
+        r.array
+    ).desc("value"),
+
+    lsbracket: () => word("["),
+    rsbracket: () => word("]"),
+    lbracket: () => word("("),
+    rbracket: () => word(")"),
+    lbrace: () => word('{'),
+    rbrace: () => word('}'),
+    comma: () => word(','),
 
     // TODO: this regexp is not perfect, is it possible to have multiple repeatable " symbols in a string?
-    quotedString: () => P.regexp(/"([^"\x00-\x1F\x7F]|[a-fA-F0-9]{4})*"/).desc("string"),
-    decimalNumber: () => P.regexp(/(\d+(\.\d*)?|\d*\.\d+)/).desc("decimal number").map(Number).desc("decimal number"),
-    number: () => P.regexp(/-?\d+/).map(Number).desc("number"),
+    quotedString: () => token(P.regexp(/"([^"\x00-\x1F\x7F]|[a-fA-F0-9]{4})*"/)).desc("string"),
+    decimalNumber: () => token(P.regexp(/(\d+(\.\d*)?|\d*\.\d+)/)).map(Number).desc("decimal number"),
+    number: () => token(P.regexp(/-?\d+/)).map(Number).desc("number"),
     array: (r) => {
-        return word("[")
-            .then(P.alt(r.quotedString, r.decimalNumber, r.number).sepBy(r._))
-            .skip(word("]"))
+        return r.lsbracket
+            .then(r.value.sepBy(r.comma))
+            .skip(r.rsbracket)
             .desc("array")
-    },
-    _: () => P.optWhitespace.desc("whitespace")
+    }
 });
 
 module.exports = QueryLang;
--- test.js
+++ test.js
@@ -1,5 +1,15 @@
 let QueryLang = require('./QueryLang');
 
-let expr = 'bc=5 a <3 g> 10 stamm = "5555" size=10 "full text search" x=[2 3 "100500"]';
-let result = QueryLang.fullExpr.tryParse(expr);
-console.log(result);
+let expr = 'bc=5 a <3 g> 10 stamm = "5555" size=10 "full text search" x=[2, 3, "100500"]';
+let expr2 = 'bc  =3 a>   10 st = "am" ';
+let expr3 = ' bc  =3 a>   10 st = "am" ';
+let expr4 = '              bc  =3 a           >   10  "3215"   st = "am" ';
+let arrayExpr1 = 'arr= [ 1,   2 ,   3 ]';
+let arrayExpr2 = 'arr=[1,2,3]';
+
+console.log(QueryLang.fullExpr.tryParse(expr));
+console.log(QueryLang.fullExpr.tryParse(expr2));
+console.log(QueryLang.fullExpr.tryParse(expr3));
+console.log(QueryLang.fullExpr.tryParse(expr4));
+console.log(QueryLang.fullExpr.tryParse(arrayExpr1));
+console.log(QueryLang.fullExpr.tryParse(arrayExpr2));
